635f934e22994ac337d85065b0e2448f4b02f0b4
Bogdan Bogdanovic <bogdi.mail@gmail.com>
2022-04-14 22:19:39 +0200

    changed from class to interface

--- models/project.ts
+++ models/project.ts
@@ -1,26 +1,10 @@
-class Project {
+interface Project {
   title: string;
   urls: { main: string; source: string };
   imageSrc: string;
   descriptionText: string;
   technologiesUsed: string[];
   buttonVariants: { live: boolean; info: boolean };
-
-  constructor(
-    title: string,
-    urls: { main: string; source: string },
-    imageSrc: string,
-    descriptionText: string,
-    technologiesUsed: string[],
-    buttonVariants: { live: boolean; info: boolean }
-  ) {
-    this.title = title;
-    this.urls = urls;
-    this.imageSrc = imageSrc;
-    this.descriptionText = descriptionText;
-    this.technologiesUsed = technologiesUsed;
-    this.buttonVariants = buttonVariants;
-  }
 }
 
 export default Project;
