b681b6a8cf10e7240d8d7c36cfe70d71b45303a7
Bogdan Bogdanovic <67063384+bbtools-ps@users.noreply.github.com>
2023-02-11 22:33:57 +0100

    updated about me

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,4 +1,5 @@
-import { useTranslation } from "next-i18next";
+import { Link } from "@nextui-org/react";
+import { Trans, useTranslation } from "next-i18next";
 import HeaderMenu from "./HeaderMenu";
 
 const Header = () => {
@@ -9,7 +10,20 @@ const Header = () => {
       <div className="content-wrap">
         <h1>{t("home:AuthorName_Label")}</h1>
         <h2>{t("home:AuthorHeadline_Label")}</h2>
-        <p>{t("home:AuthorAbout_Label")}</p>
+        <p style={{ whiteSpace: "pre-wrap" }}>
+          <Trans
+            i18nKey={t("home:AuthorAbout_Label")}
+            components={{
+              link1: (
+                <Link
+                  href="mailto:bogdi.mail@gmail.com"
+                  target="_top"
+                  rel="noopener noreferrer"
+                />
+              ),
+            }}
+          />
+        </p>
       </div>
     </header>
   );
