41ae53ae3076ee1fbd60e206fdbabf92fc52ad36
Bogdan Bogdanovic <67063384+bbtools-ps@users.noreply.github.com>
2023-03-29 00:08:48 +0200

    update for crossOrigin

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Lora&display=swap" rel="stylesheet" />
         <link rel="shortcut icon" type="image/svg+xml" href="./favicon.svg" />
       </Head>
