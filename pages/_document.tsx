afd6b14c64d68f9776948ad5b995081246b1ca14
Bogdan Bogdanovic <67063384+bbtools-ps@users.noreply.github.com>
2023-04-11 20:31:59 +0200

    updated Document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,35 @@
-import { Head, Html, Main, NextScript } from "next/document";
+import { CssBaseline } from "@nextui-org/react";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import React from "react";
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Lora&display=swap" rel="stylesheet" />
-        <link rel="shortcut icon" type="image/svg+xml" href="./favicon.svg" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: React.Children.toArray([initialProps.styles])
+    };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lora&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="shortcut icon" type="image/svg+xml" href="./favicon.svg" />
+          {CssBaseline.flush()}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
