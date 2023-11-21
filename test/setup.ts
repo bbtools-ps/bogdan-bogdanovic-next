99158ea81907aead4add49c978a387121d8b87a0
Bogdan Bogdanovic <67063384+bbtools-ps@users.noreply.github.com>
2023-11-21 22:35:40 +0100

    tests cleanup

--- test/setup.ts
+++ test/setup.ts
@@ -1,21 +1,8 @@
 import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
-import { afterEach, vi } from "vitest";
+import { afterEach } from "vitest";
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
   cleanup();
 });
-
-const matchMediaMock = vi.fn((query: unknown) => ({
-  matches: false,
-  media: query,
-  onchange: null,
-  addListener: vi.fn(),
-  removeListener: vi.fn(),
-  addEventListener: vi.fn(),
-  removeEventListener: vi.fn(),
-  dispatchEvent: vi.fn()
-}));
-
-vi.stubGlobal("matchMedia", matchMediaMock);
