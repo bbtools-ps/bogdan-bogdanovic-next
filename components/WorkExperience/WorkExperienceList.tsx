6c7279db9d575a84faab919235b5a75a2ff70e71
Bogdan Bogdanovic <67063384+bbtools-ps@users.noreply.github.com>
2024-05-09 17:27:46 +0200

    changed the width of the company link to only occupy the text

--- components/WorkExperience/WorkExperienceList.tsx
+++ components/WorkExperience/WorkExperienceList.tsx
@@ -31,7 +31,7 @@ export default async function WorkExperienceList({
             <h3>{experience.fields.jobTitle.stringValue}</h3>
             <Link
               href={experience.fields.companyLink?.stringValue || "#"}
-              className="mb-4 block text-xl font-bold text-blue-700 duration-100 hover:text-blue-400 dark:text-blue-200 dark:hover:text-blue-400 lg:mb-0"
+              className="mb-4 inline-block text-xl font-bold text-blue-700 duration-100 hover:text-blue-400 dark:text-blue-200 dark:hover:text-blue-400 lg:mb-0"
               rel="noopener noreferrer"
               target="_blank"
             >
