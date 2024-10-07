f980cc1cd2afc9cbf9ed401cb5ac042a4edc568e
Bogdan Bogdanovic <bogdi.mail@gmail.com>
2024-10-07 16:27:21 +0200

    fixed content contained within landmarks issue

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,6 +5,8 @@ import Languages from "@/components/Languages/Languages";
 import TranslationsProvider from "@/components/TranslationsProvider";
 import WorkExperience from "@/components/WorkExperience/WorkExperience";
 import initTranslations from "@/lib/i18n";
+import Image from "next/image";
+import bgImage from "../../public/images/bg.webp";
 
 const i18nNamespaces = ["home"];
 
@@ -26,6 +28,17 @@ export default async function Page({
       <WorkExperience locale={locale} />
       <Education locale={locale} />
       <Languages locale={locale} />
+      <div className="fixed inset-0 -z-10 h-full w-full">
+        <Image
+          src={bgImage}
+          alt="Background image"
+          fill
+          placeholder="blur"
+          className="object-cover"
+        />
+        <div className="absolute h-full w-full bg-slate-100 opacity-80 dark:bg-slate-800" />
+        <div className="absolute h-full w-full overflow-auto bg-gradient-to-t from-slate-100/50 dark:from-slate-900/50" />
+      </div>
     </TranslationsProvider>
   );
 }
